7b733472dbcc1b5633fe225e1aa6e19891cafd76
Camden <camden.kay12@yahoo.com>
2022-07-13 18:37:40 -0400

    Update RDS

--- generate_headshot_rds.R
+++ generate_headshot_rds.R
@@ -1,5 +1,5 @@
 
-headshots <- readr::read_csv("https://github.com/camdenk/mlbplotR-data/raw/main/Player-IDs.csv")
+headshots <- readr::read_csv("./Player-IDs.csv")
 
 saveRDS(headshots, "./Player-IDs.rds")
 
